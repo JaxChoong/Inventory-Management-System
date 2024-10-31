d754de666fe0415db75e9837b75361f6d04653e7
Jax Choong <choongjax@gmail.com>
2024-10-31 21:50:41 +0800

    feat: now can extract headers

--- items.cpp
+++ items.cpp
@@ -3,12 +3,21 @@
 class Items{
     public:
         std::vector<std::string> inventory;
+        std::vector<std::string> header;
         void addInventory(std::string item){
             inventory.push_back(item);
         }
         void printInventory(){
             for(auto item : inventory){
                 std::cout << item << std::endl;
+            }
+        }
+        void addHeader(std::string column){
+            header.push_back(column);
+        }
+        void printHeader(){
+            for(auto column : header){
+                std::cout << column << std::endl;
             }
         }
 };
--- main.cpp
+++ main.cpp
@@ -2,12 +2,24 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 #include "Items.h"
 
 int main(){
     std::ifstream file("stock.csv");
     std::string line;
     Items Items;
+
+    // Read the first line as the header
+    if (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string column;
+        while (std::getline(ss, column, ',')) {
+            Items.addHeader(column);
+        }
+        Items.addInventory(line);
+    }
+
     while(std::getline(file, line)){
         Items.addInventory(line);
     }
